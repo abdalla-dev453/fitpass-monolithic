8cc2b44807979491c0641358e70262a09c50b594
abdalla-dev453 <abdallamsema69@gmail.com>
2026-09-03 01:13:11 +0300

    Fix relative import in server/models/user.py

--- server/models/user.py
+++ server/models/user.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from extensions import db
-from server.models import nutrition_profile
+from models.nutrition_profile import NutritionProfile
 
 
 class User(db.Model):
